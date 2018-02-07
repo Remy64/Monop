e8e50a9d14903d6bdcfda5e16a7f6018d0ea9999
jb332 <36227037+jb332@users.noreply.github.com>
2018-02-07 16:09:49 +0100

    Update joueur.ads

--- joueur.ads
+++ joueur.ads
@@ -1,9 +1,8 @@
 package Joueur is
    subtype Positive in 1..40
-   Nb_Rues : constant Positive := 28 ;
+   Numero_Joueur : Positive ;
    type T_Proprietes is array 1..Nb_Rues of Cases ;
    type Un_Pion is record
-      
       Compte : Natural ;
       Proprietes : T_Proprietes ;
    end record ;
