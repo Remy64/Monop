0ae9170e5a6ed446dd4f10636fc35eab6630dbe0
jb332 <36227037+jb332@users.noreply.github.com>
2018-02-07 16:06:38 +0100

    Update cases.ads

--- cases.ads
+++ cases.ads
@@ -1,23 +1,22 @@
 package Cases is
-   type Fonction_Case is (Gare, Service, Rue, Prison, Rien, Visite, Pioche, Taxe, Depart) ;
-   type Couleur_Case is (Rien, Marron, Bleu_Clair, Rose, Orange, Rouge, Jaune, Vert, Bleu) ;
-   type Loyers is record
-      Terrain_Nu : Positive ;
-      1_Maison : Positive ;
-      2_Maisons : Positive ;
-      3_Maisons : Positive ;
-      4_Maisons : Positive ;
-      Hotel : Positive ;
+   type Fonction_Case is (Gare, Service, Rue, Prison, Rien, Pioche, Taxe, Depart) ; -- énumère les fonctions possibles des cases
+   type Couleur_Case is (Rien, Marron, Bleu_Clair, Rose, Orange, Rouge, Jaune, Vert, Bleu) ; -- liste les couleurs des terrains
+   type Loyers is record -- liste les loyers d'une case en fonction des possessions
+      Terrain_Nu : Positive ; -- loyer si terrain nu
+      1_Maison : Positive ; -- loyer si 1 maison
+      2_Maisons : Positive ; -- loyer si 2 maisons
+      3_Maisons : Positive ; -- loyer si 3 maisons
+      4_Maisons : Positive ; -- loyer si 4 maisons
+      5_Maisons : Positive ; -- loyer si 5 maisons
    end record ;
    
    type Cases is record
-      Nom : String ;
-      Prix_Terrain : Positive ;
-      Prix_Maison : Positive ;
-      Hypotheque : Positive ;
-      Loyer : Loyers ;
-      Fonction : Fonction_Case ;
-      Couleur : Couleur_Case ;
-      Occupe : Boolean ;
-      Occupant : Pion ; 
+      Nom : String ; -- nom de la case
+      Prix_Terrain : Positive ; -- prix d'achat de la case si achetable
+      Prix_Maison : Positive ; -- prix de construction d'une maison si constructible
+      Loyer : Loyers ; -- donne les loyers
+      Fonction : Fonction_Case ; -- donne la fonction de la case
+      Couleur : Couleur_Case ; -- donne la couleur de la case
+      Occupe : Boolean ; -- vrai si la case est occupée par un joueur, faux sinon
+      Occupant : Positive ; -- donne le numéro du joueur qui occupe la case 
    end record ;
