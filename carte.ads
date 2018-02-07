ed4ba2e3728da370243adc6853b9543fb57e6e69
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-02-07 16:01:13 +0100

    Update carte.ads

--- carte.ads
+++ carte.ads
@@ -1,20 +1,31 @@
-package Cartes is
-   Nb_Cartes : constant Positive := 50 ;
-   
-   type Un_Effet is (Argent, Prison, Bouger) ;
-   
-   type Une_Carte is record
-      Titre : String ;
-      Description : String ;
-      Effet : Un_Effet ;
-      Montant : Integer ;
-   end record ;
-   
-   type Pile_De_Cartes is array 1..Nb_Cartes of Une_Carte ;
-   
-   Pile : Pile_De_Cartes ;
-     
-   procedure Passer_Dessous(Pile : in out Pile_De_Cartes) ;
-   function Piocher_Carte(Pile : in Pile_De_Cartes) ;
-   
-end Cartes ;
+WITH Joueur ;
+use Joueur ;
+PACKAGE Cartes IS
+
+      Nb_Cartes : constant Positive := 50 ; --Nombre
+
+      type Un_Effet is (Argent, Prison, Bouger) ; --Effet de la carte
+
+      TYPE Une_Carte IS RECORD
+         Titre : String(1..20) ;
+         Description : String(1..100) ;
+         Effet : Un_Effet ;
+         Montant : Integer ;
+      END RECORD ;
+
+      TYPE T_Cartes IS ARRAY (1..Nb_Cartes) OF Une_Carte ; -- Tableau de cartes
+
+      TYPE Pile_Carte IS RECORD -- File de cartes
+         Tab : T_Cartes ;
+         Deb : Natural ;
+         Fin : Natural ;
+
+      END RECORD ;
+
+
+
+
+      procedure Piocher_Carte (Pile : in out Pile_Carte; Joueur : Joueur) ;-- Applique l'effet de la carte au sommet de la pile
+      procedure Tomber_Case_Chance(Pile : in out Pile_Carte; Joueur : Joueur) ; --Pioche une carte, applique son effet et la place sous la pile
+
+   end Cartes ;
