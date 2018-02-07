7af0cb64cb9dacc92feb8b21800dcb84a09770fb
mickey6934 <36227049+mickey6934@users.noreply.github.com>
2018-02-07 16:18:52 +0100

    Update commerce.ads

--- commerce.ads
+++ commerce.ads
@@ -1,6 +1,6 @@
 package Commerce is
-   procedure Hyothequer(J : Joueur, N : Numero_Case) ;
-   procedure Acheter_Terrain(J : Joueur, N : Numero_Case) ;
-   procedure Acheter_Maison(J : Joueur, N : Numero_Case) ;
-   procedure Vendre_Maison(J : Joueur, N : Numero_Case) ;
+   procedure Hypothequer(J : Joueur, N : Numero_Case) ; -- le joueur proprietaire perçoit la moitie de la valeur d'achat de la propriete & le loyer du terrain nu = 0 ; 
+   procedure Acheter_Terrain(J : Joueur, N : Numero_Case) ; -- le joueur doit tomber sur la propriete pour l'acheter ; 
+   procedure Acheter_Maison(J : Joueur, N : Numero_Case) ; -- possible uniquement quand le joueur possede les 3 proprietes d'une meme couleur et uniquement quand c'est a son tour de jouer ; 
+   procedure Vendre_Maison(J : Joueur, N : Numero_Case) ; -- possible uniquement quand c'est a son tour de jouer ; 
 end Commerce ;
