394b2199d0ffb4b1a9316477de01333fa3f84bad
mickey6934 <36227049+mickey6934@users.noreply.github.com>
2018-02-07 16:32:12 +0100

    Update de.ads

--- de.ads
+++ de.ads
@@ -1,4 +1,5 @@
 package De is
-   subtype Valeurs_Des is Positive range 1..12 ;
-   function Lancer return Valeurs_Des ;   
+   subtype Valeurs_Des is Positive range 1..6 ;
+   function Lancer return Valeurs_De ;  
+   function Double return Boolean ; -- Le joueur rejoue une deuxieme fois lorsque il a fait un double ; 
 end De ;
