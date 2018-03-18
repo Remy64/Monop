553c639349f02ec48cc366be132d6f1ca68765c6
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-03-18 19:34:41 +0100

    Update joueur.ads

--- joueur.ads
+++ joueur.ads
@@ -1,12 +1,12 @@
-with Cases ;
-use Cases ;
+package Joueur is
+   Numero_Joueur : Positive ;
+   type T_Proprietes is array 1..Nb_Rues of Cases ;
+   type Un_Pion is record
+      Compte : Natural ;
+      Proprietes : T_Proprietes ;
+      -- ajouter position
+   end record ;
 
-package Plateau is
 
-   subtype Numero_Case is Positive in 1..36 ; -- correspond aux numÃ©ros des cases de 1 Ã  36 (car 36 cases)
-   type Plateau is array 1..36 of Cases ; -- dÃ©finit un tableau composÃ© des 36 cases du plateau (le type "Cases" est dÃ©fini dans le paquetage "Cases")
-   procedure Bouger(N : Integer, J : Joueur) ; -- permet de se dÃ©placer d'un certain nombre de cases (un nbre nÃ©gatif permet de reculer)
-   procedure Aller_A(N : Numero_Case, J : Joueur) ; -- permet d'atteindre une case particuliÃ¨re
-   procedure Initialiser ; -- permet d'initialiser le plateau avec tous les joueurs sur la case dÃ©part
 
-end Plateau ;
+end Joueur ;
