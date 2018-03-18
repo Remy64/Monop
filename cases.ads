e405959f16c29bd1411867850114fb469c372bdc
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-03-18 19:32:11 +0100

    Update cases.ads

--- cases.ads
+++ cases.ads
@@ -1,7 +1,7 @@
 package Cases is
-   type Fonction_Case is (Gare, Service, Rue, Prison, Rien, Pioche, Taxe, Depart) ; -- énumère les fonctions possibles des cases
-   type Couleur_Case is (Rien, Marron, Bleu_Clair, Rose, Orange, Rouge, Jaune, Vert, Bleu) ; -- liste les couleurs des terrains
-   type Loyers is record -- liste les loyers d'une case en fonction des possessions
+   type Fonction_Case is (Gare, Service, Rue, Prison, Rien, Pioche, Taxe, Depart) ; -- Ã©numÃ¨re les fonctions possibles des cases
+   type Couleur_Case is (Rien, Marron, Bleu_Clair, Rose, Orange, Rouge, Jaune, Vert, Bleu) ; -- type record RGB
+   type Loyers is record -- liste les loyers d'une case en fonction des possessions faire tableau
       Terrain_Nu : Positive ; -- loyer si terrain nu
       1_Maison : Positive ; -- loyer si 1 maison
       2_Maisons : Positive ; -- loyer si 2 maisons
@@ -9,13 +9,15 @@ package Cases is
       4_Maisons : Positive ; -- loyer si 4 maisons
       5_Maisons : Positive ; -- loyer si 5 maisons
    end record ;
-   
+
    type Cases is record
-      Nom : String ; -- nom de la case
-      Prix_Terrain : Positive ; -- prix d'achat de la case si achetable
+      Nom : String ; -- faire avec un pointeur
+      Prix_Terrain : Natural ; -- prix d'achat de la case si achetable
       Prix_Maison : Positive ; -- prix de construction d'une maison si constructible
       Loyer : Loyers ; -- donne les loyers
       Fonction : Fonction_Case ; -- donne la fonction de la case
       Couleur : Couleur_Case ; -- donne la couleur de la case
-      Occupant : Natural ; -- donne le numéro du joueur qui occupe la case, vaut 0 si la case est inoccupée
+      Occupant : Natural ; -- à modifier, ne pas le mettre là
    end record ;
+
+-- fonctions : case achetable, loyer, boolean pour dessiner la couleur
