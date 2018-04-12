3352b1a6f9cdae0e0cc43f7cce935b334e7a2b0e
Remy64 <35994712+Remy64@users.noreply.github.com>
2018-04-12 14:11:31 +0000

    Delete de.ads

--- de.ads
+++
@@ -1,9 +0,0 @@
-package De is
-   SUBTYPE Valeurs_Des IS Positive RANGE 1..6 ;
-   --type record contient somme des dés + boolean pour le double
-   function Lancer return Valeurs_De ; -- renvoie le record
-   FUNCTION Double RETURN Boolean ; -- Le joueur rejoue une deuxieme fois lorsque il a fait un double ;
-
--- Lancer déterministe
-
-end De ;
