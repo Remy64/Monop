6446f167bb1843c6462aad1e1a83ab7d1a340722
jb332 <36227037+jb332@users.noreply.github.com>
2018-04-12 19:59:39 +0200

    Add files via upload

--- listes_proprietes.adb
+++ listes_proprietes.adb
@@ -1,6 +1,3 @@
-with Plateau;
-use Plateau;
-
 package body Listes_Proprietes is
    
    function Possede_Propriete(L : Liste_Proprietes ; C : Numero_Case) return Boolean is
@@ -8,19 +5,19 @@ package body Listes_Proprietes is
      return True;
    end Possede_Propriete;
    
-   procedure Ajouter_Propriete(L : in out Liste) is
+   procedure Ajouter_Propriete(L : in out Liste_Proprietes) is
    begin
       null;
    end Ajouter_Propriete;
 
-   procedure Supprimer_Propriete(L : in out Liste) is
+   procedure Supprimer_Propriete(L : in out Liste_Proprietes) is
    begin
       null;
    end Supprimer_Propriete;
 
-   function Nb_Maisons_Propriete(L : Liste_Propriete ; C : Numero_Case) return N_Maison is
+   function Nb_Maisons_Propriete(L : Liste_Proprietes ; C : Numero_Case) return N_Maison is
    begin
       return 0;
-   end Nb_Masisons_Propriete;
+   end Nb_Maisons_Propriete;
 
 end Listes_Proprietes;
--- listes_proprietes.ads
+++ listes_proprietes.ads
@@ -1,16 +1,15 @@
-with Plateau;
-use Plateau;
-
 package Listes_Proprietes is
    type Liste_Proprietes is private;
+   subtype Numero_Case is Positive range 1..36;
+   subtype N_Maison is Natural range 0..5;
    
-   function Possede_Propriete(L : Liste_Proprietes, C : Numero_Case) return Boolean;
+   function Possede_Propriete(L : Liste_Proprietes ; C : Numero_Case) return Boolean;
    
-   procedure Ajouter_Propriete(L : in out Liste);
+   procedure Ajouter_Propriete(L : in out Liste_Proprietes);
    
-   procedure Supprimer_Propriete(L : in out Liste);
+   procedure Supprimer_Propriete(L : in out Liste_Proprietes);
    
-   function Nb_Maisons_Propriete(L : Liste_Propriete, C : Numero_Case) return N_Maison;
+   function Nb_Maisons_Propriete(L : Liste_Proprietes ; C : Numero_Case) return N_Maison;
 
 private
    type Cellule;
@@ -18,7 +17,7 @@ private
    
    type Une_Propriete is record
       Num_Case : Numero_Case;
-      Nb_Maisons : Un_Nb_Maisons;
+      Nb_Maisons : N_Maison;
       Hypotheque : Boolean;
    end record;
    
