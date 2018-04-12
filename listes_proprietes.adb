425d867ebd7e51ca0169335daf66d7f7530acdef
jb332 <36227037+jb332@users.noreply.github.com>
2018-04-12 20:38:06 +0200

    Add files via upload

--- listes_proprietes.adb
+++ listes_proprietes.adb
@@ -1,18 +1,37 @@
+with Ada.Unchecked.Deallocation;
+
 package body Listes_Proprietes is
    
    function Possede_Propriete(L : Liste_Proprietes ; C : Numero_Case) return Boolean is
+      Trouve : Boolean;
+      Aux : Liste_Proprietes;
    begin
-     return True;
+      Trouve := False;
+      Aux := L;
+      while Aux /= null loop
+	 if Aux.all.Propriete.Num_Case = C then
+	    Trouve := True;
+	 end if;
+	 Aux := Aux.all.Suiv;
+      end loop;
+      return Trouve;
    end Possede_Propriete;
    
-   procedure Ajouter_Propriete(L : in out Liste_Proprietes) is
+   procedure Ajouter_Propriete(C : in Numero_Case ; L : in out Liste_Proprietes) is
    begin
-      null;
+      L := new Cellule'((C, 0, False), L);
    end Ajouter_Propriete;
 
-   procedure Supprimer_Propriete(L : in out Liste_Proprietes) is
+   procedure Supprimer_Propriete(C : in Numero_Case ; L : in out Liste_Proprietes) is
+      Aux : Liste_Proprietes;
    begin
-      null;
+      if L.all.Propriete.Num_Case = C then
+	 Aux := L;
+	 L := L.all.Suiv;
+	 Free(Aux);
+      else
+	 Supprimer_Propriete(C, L.all.Suiv);
+      end if;
    end Supprimer_Propriete;
 
    function Nb_Maisons_Propriete(L : Liste_Proprietes ; C : Numero_Case) return N_Maison is
--- listes_proprietes.ads
+++ listes_proprietes.ads
@@ -1,13 +1,16 @@
 package Listes_Proprietes is
+   
+   procedure Free is new Unchecked_Deallocation(Cellule, Liste_Proprietes);
+   
    type Liste_Proprietes is private;
    subtype Numero_Case is Positive range 1..36;
    subtype N_Maison is Natural range 0..5;
    
    function Possede_Propriete(L : Liste_Proprietes ; C : Numero_Case) return Boolean;
    
-   procedure Ajouter_Propriete(L : in out Liste_Proprietes);
+   procedure Ajouter_Propriete(C : in Numero_Case ; L : in out Liste_Proprietes);
    
-   procedure Supprimer_Propriete(L : in out Liste_Proprietes);
+   procedure Supprimer_Propriete(C : in Numero_Case ; L : in out Liste_Proprietes);
    
    function Nb_Maisons_Propriete(L : Liste_Proprietes ; C : Numero_Case) return N_Maison;
 
