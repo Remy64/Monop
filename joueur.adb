a962428bb0f28c47779380694f23fe12946d9f6c
jb332 <36227037+jb332@users.noreply.github.com>
2018-04-21 20:12:39 +0200

    Add files via upload

--- joueur.adb
+++ joueur.adb
@@ -2,22 +2,23 @@ with Liste_Proprietes;
 use Liste_Proprietes;
 
 package body Joueur is
-   subtype Un_Nb_Joueurs is Positive range 1..6;
    
-   type Un_Joueur is record
-      Compte : Natural;
-      Proprietes : Liste_Proprietes;
-   end record;
-   
-   type Un_Tab_Joueurs is array 1..Un_Nb_Joueurs of Un_Joueur;
-   
-   procedure Init_Joueurs(Joueurs : Un_Tab_Joueurs) is
+   procedure Init_Joueurs is
    begin
       for I in Joueurs'Range loop
 	 Joueurs(I).Compte := 1500;
 	 Init_Liste_Proprietes(Joueurs(I).Proprietes);
       end loop;
    end Init_Joueurs;
-
-
-end Joueur;
+   
+   function Compte_Joueur(N : Un_Num_Joueur) return Natural is
+   begin
+      return Joueurs(N).Compte;
+   end Compte_Joueur;
+   
+   function Proprietes_Joueur(N : Un_Num_Joueur) return Liste_Proprietes is
+   begin
+      return Joueurs(N).Proprietes;
+   end Proprietes_Joueur;
+   
+   end Joueur;
--- joueur.ads
+++ joueur.ads
@@ -2,20 +2,24 @@ with Liste_Proprietes;
 use Liste_Proprietes;
 
 package Joueur is
-   subtype Un_Nb_Joueurs is Positive range 1..6;
    
-   Nb_Joueurs : Un_Nb_Joueurs := constant 6;
+   Nb_Joueurs : Positive := constant 6;
    
    type Un_Joueur is record
       Compte : Natural;
       Proprietes : Liste_Proprietes;
    end record;
    
-   type Un_Tab_Joueurs is array 1..Un_Nb_Joueurs of Un_Joueur;
+   type Un_Tab_Joueurs is array 1..Nb_Joueurs of Un_Joueur;
    
    Joueurs : Un_Tab_Joueurs;
    
-   procedure Init_Joueurs(Joueurs : Un_Tab_Joueurs);
-
-
+   subtype Un_Num_Joueur is Positive range Joueurs'Range;
+   
+   procedure Init_Joueurs;
+   
+   function Compte_Joueur(N : Un_Num_Joueur) return Natural;
+   
+   function Proprietes_Joueur(N : Un_Num_Joueur) return Liste_Proprietes;
+   
 end Joueur;
