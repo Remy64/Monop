b338fe582223157b42013a518131c17529496423
jb332 <36227037+jb332@users.noreply.github.com>
2018-04-21 19:12:22 +0200

    Add files via upload

--- joueur.adb
+++ joueur.adb
@@ -1,10 +1,8 @@
 with Liste_Proprietes;
 use Liste_Proprietes;
 
-package Joueur is
+package body Joueur is
    subtype Un_Nb_Joueurs is Positive range 1..6;
-   
-   Nb_Joueurs : Un_Nb_Joueurs := constant 6;
    
    type Un_Joueur is record
       Compte : Natural;
@@ -13,9 +11,13 @@ package Joueur is
    
    type Un_Tab_Joueurs is array 1..Un_Nb_Joueurs of Un_Joueur;
    
-   Joueurs : Un_Tab_Joueurs;
-   
-   procedure Init_Joueurs(Joueurs : Un_Tab_Joueurs);
+   procedure Init_Joueurs(Joueurs : Un_Tab_Joueurs) is
+   begin
+      for I in Joueurs'Range loop
+	 Joueurs(I).Compte := 1500;
+	 Init_Liste_Proprietes(Joueurs(I).Proprietes);
+      end loop;
+   end Init_Joueurs;
 
 
 end Joueur;
