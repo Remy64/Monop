92ec05b59dbfc02a4ce9e48eda59b537d0ae234c
jb332 <36227037+jb332@users.noreply.github.com>
2018-04-21 19:16:24 +0200

    Add files via upload

---
+++ main.adb
@@ -0,0 +1,38 @@
+with Banque, Commerce, Des, Des_Cases, Files_Cartes, Joueur, Liste_Proprietes, Un_Plateau;
+use Banque, Commerce, Des, Des_Cases, Files_Cartes, Joueur, Liste_Proprietes, Un_Plateau;
+
+procedure Main is
+   
+   subtype Un_Nb_Joueurs is Positive range 1..6;
+   Nb_Joueurs : Un_Nb_Joueurs;
+   Cond1, Cond2, Cond3 : Boolean;
+   
+begin
+   
+   while Cond loop
+      
+      for I in Joueurs'Range loop
+	 
+         if Cond1 then
+	    null;
+	 else
+	    null;
+	 end if;
+	 
+	 if Cond2 then
+	    null;
+	 else
+	    null;
+	 end if;
+	 
+	 if Cond3 then
+	    null;
+	 else
+	    null;
+	 end if;
+	 
+      end loop;
+      
+   end loop;
+   
+end Main;
