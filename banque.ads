0d49c55a072b3eb70777d46ffd914deb48ecb97b
jb332 <36227037+jb332@users.noreply.github.com>
2018-04-24 19:02:43 +0200

    Delete banque.ads

--- banque.ads
+++
@@ -1,7 +0,0 @@
-   package Banque is
-
-   PROCEDURE Retirer(N : Positive; J : IN OUT Joueur) ; -- Faire une seule procedure + une exception
-   PROCEDURE Renflouer(N : Positive; J : IN OUT Joueur) ; -- Octroie N argent Ã  un joueur
-
-   end Banque ;
-
